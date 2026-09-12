5a16558ab1374e4764b4bc6593df7f0b91448912
Talles Souza <tallessouza117@gmail.com>
2026-09-12 20:38:44 -0300

    feat: cria mapeamento jpa e repositorios
    - Mapeia as entidades Usuario, Orientador, PerfilOrientador e LinhaPesquisa utilizando JPA e Lombok.
    - Cria as interfaces de repository (LinhaPesquisaRepository, OrientadorRepository, PerfilOrientadorRepository) estendendo JpaRepository para o domínio de Orientador.

---
+++ src/backend/gestao-tcc/src/main/java/br/edu/ifsc/gestao_tcc/model/Orientador.java
@@ -0,0 +1,21 @@
+package br.edu.ifsc.gestao_tcc.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "orientadores")
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orientador extends Usuario {
+
+    @Column(length = 100)
+    private String departamento;
+
+    @OneToOne(mappedBy = "orientador", cascade = CascadeType.ALL)
+    private PerfilOrientador perfil;
+}
