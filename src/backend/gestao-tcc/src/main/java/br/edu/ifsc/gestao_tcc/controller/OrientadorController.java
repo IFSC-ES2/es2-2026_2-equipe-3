f234833fa95ea3134d964f7cafc429ca9e21a216
EduCardoso22 <eduardo.co22@aluno.ifsc.edu.br>
2026-09-13 16:04:37 -0300

    feat: implementa endpoint POST /orientadores retornando status 201 e header Location

--- src/backend/gestao-tcc/src/main/java/br/edu/ifsc/gestao_tcc/controller/OrientadorController.java
+++ src/backend/gestao-tcc/src/main/java/br/edu/ifsc/gestao_tcc/controller/OrientadorController.java
@@ -1,19 +1,35 @@
 package br.edu.ifsc.gestao_tcc.controller;
 
 import br.edu.ifsc.gestao_tcc.dto.OrientadorRequestDTO;
+import br.edu.ifsc.gestao_tcc.dto.OrientadorResponseDTO;
 import br.edu.ifsc.gestao_tcc.dto.OrientadorUpdateDTO;
+import br.edu.ifsc.gestao_tcc.model.Orientador;
+import br.edu.ifsc.gestao_tcc.service.OrientadorService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/orientadores")
+@RequiredArgsConstructor
 public class OrientadorController {
+
+    private final OrientadorService orientadorService;
 
     @PostMapping
     public ResponseEntity<Void> cadastraOrientador(@RequestBody @Valid OrientadorRequestDTO orientadorDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        Orientador orientadorCriado = orientadorService.cadastrar(orientadorDTO);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(orientadorCriado.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping
@@ -22,15 +38,15 @@ public class OrientadorController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Void> ListaOrientadorID(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<OrientadorResponseDTO> ListaOrientadorID(@PathVariable Long id) {
+        OrientadorResponseDTO orientadorResponse = orientadorService.buscarPorId(id);
+        return ResponseEntity.ok(orientadorResponse);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Void> atualizaOrientadorID(
             @PathVariable Long id,
             @RequestBody @Valid OrientadorUpdateDTO orientadorDTO) {
-
         return ResponseEntity.ok().build();
     }
 
