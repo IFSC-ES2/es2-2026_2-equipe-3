9deece337e83b859611d5af21b0005a464d34959
Eduardo Cardoso <eduardo.co22@aluno.ifsc.edu.br>
2026-09-24 13:34:33 -0300

    feat: adicionar exceção para operações com orientadores inativos

---
+++ SIGTCC/backend/gestao-tcc/src/main/java/br/edu/ifsc/gestao_tcc/exception/OrientadorInativoException.java
@@ -0,0 +1,7 @@
+package br.edu.ifsc.gestao_tcc.exception;
+
+public class OrientadorInativoException extends RegraDeNegocioException {
+    public OrientadorInativoException(String message) {
+        super(message);
+    }
+}
