160c3430c950b5bdd31bd9bd7fbc15ee775e936a
Eduardo Cardoso <eduardo.co22@aluno.ifsc.edu.br>
2026-09-24 13:36:46 -0300

    feat: adicionar exceção base para violações de regras de negócio (HTTP 422)

---
+++ SIGTCC/backend/gestao-tcc/src/main/java/br/edu/ifsc/gestao_tcc/exception/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package br.edu.ifsc.gestao_tcc.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
